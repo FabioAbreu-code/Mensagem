58f339b2928df1845e99f65bbb820a8907650820
Fabio Abreu <fabiofiqueli@hotmail.com>
2021-04-21 10:53:11 +0100

    Obter, mostrar e passar Mensagem

--- app/src/main/java/pt/ipg/mensagem/MainActivity.kt
+++ app/src/main/java/pt/ipg/mensagem/MainActivity.kt
@@ -1,8 +1,11 @@
 package pt.ipg.mensagem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +13,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun enviaMensagem(view: View) {}
+    fun enviaMensagem(view: View) {
+        val editTextMensagem = findViewById<EditText>(R.id.editTextMensagem)
+
+        val mensagem = editTextMensagem.text.toString()
+
+        val  intent = Intent(this, MostraMensagemActivity::class.java)
+        intent.putExtra("MENSAGEM", mensagem)
+        startActivity(intent)
+
+
+    }
 }
