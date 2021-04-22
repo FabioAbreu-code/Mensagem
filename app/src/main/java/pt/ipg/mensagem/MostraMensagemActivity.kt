107fedd41483920afc645ba2da783e6c53a3c402
Fabio Abreu <fabiofiqueli@hotmail.com>
2021-04-22 21:42:17 +0100

    Está a funcionar

--- app/src/main/java/pt/ipg/mensagem/MostraMensagemActivity.kt
+++ app/src/main/java/pt/ipg/mensagem/MostraMensagemActivity.kt
@@ -1,11 +1,16 @@
 package pt.ipg.mensagem
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MostraMensagemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostra_mensagem)
+
+        val mensagem = intent.getStringExtra("MENSAGEM")
+        val textViewMensagem = findViewById<TextView>(R.id.textViewMensagem)
+        textViewMensagem.setText(mensagem)
     }
 }
